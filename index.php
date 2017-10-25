c85a7850389ff12894b266b6ae6554c5732fead2
danbond85@mail.ru <danbond85@gmail.com>
2017-10-25 20:39:04 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php 
+    $animals = [
+        'Africa' => [
+            'African buffalo',
+            'Giant river hog',
+            'Impala'
+        ],
+        'Australia' => [
+            'Echidna',
+            'Kangaroo',
+            'Tasmanian tiger'
+        ],
+        'Eurasia' => [
+            'Blind mole',
+            'Indian elephant',
+            'Moose'
+        ],
+        'North America' => [
+            'Racoon',
+            'Gray fox',
+            'Eastern chipmunk'   
+        ],
+        'South America' => [
+            'Armadillo',
+            'Ocelot',
+            'Pampas deer'
+        ]
+    ];
