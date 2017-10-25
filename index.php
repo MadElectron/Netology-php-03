b934eb7a857517817951695450b892af05eb0994
danbond85@mail.ru <danbond85@gmail.com>
2017-10-25 21:37:19 +0300

    basic variant released

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php 
+
     $animals = [
         'Africa' => [
             'African buffalo',
@@ -26,3 +27,33 @@
             'Pampas deer'
         ]
     ];
+
+    $animalsDouble = [];
+
+    foreach($animals as $continent) {
+        foreach ($continent as $animal) {
+            if (count(explode(' ', $animal)) == 2 ) {
+                $animalsDouble[] = $animal;
+            }
+        }
+    }
+
+    $firstNames = [];
+    $lastNames = [];
+
+    foreach($animalsDouble as $animal) {
+        list($firstName, $lastName) = explode(' ', $animal);
+        $firstNames[] = $firstName;
+        $lastNames[] = $lastName;
+    }
+
+    shuffle($firstNames);
+    shuffle($lastNames);
+
+    $fantasticAnimals = [];
+
+    foreach (array_combine($firstNames, $lastNames) as $firstName => $lastName) {
+        $fantasticAnimals[] = "$firstName $lastName";
+    }
+
+    echo implode(', ', $fantasticAnimals);
